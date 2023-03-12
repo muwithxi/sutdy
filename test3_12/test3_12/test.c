dd0b6211ea2205222df20b6509060d1a29c5ad98
muwithxi <muwithxisjr@qq.com>
2023-03-12 21:36:11 +0800

    浮点数存储练习

---
+++ test3_12/test3_12/test.c
@@ -0,0 +1,41 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+
+	return 0;
+}
+
+//int main()
+//{
+//	unsigned char a[1000];
+//	int i;
+//	for(i=0; i<1000; i++)
+//	{
+//		a[i] = 1+i;//±éÀúÊý×é
+//	}
+//	printf("%d", strlen(a));
+//	return 0;
+//}
+
+//int main()
+//{
+//	int n = 9;
+//	float* pfloat = (float*)&n;
+//	printf("%d\n", n);
+//	printf("%lf\n", *pfloat);
+//
+//	*pfloat = 9.0;
+//	printf("%d\n", n);
+//	printf("%lf\n", *pfloat);
+//	return 0;
+//}
+
+//int main()
+//{
+//	float a = 5.0;
+//	return 0;
+//}
