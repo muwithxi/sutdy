6832e9baf08b1cd2ddbc268dfdcdf904812b5a06
muwithxi <muwithxisjr@qq.com>
2023-03-28 23:17:50 +0800

    c语言学习6

---
+++ test_3_28/test_3_28/test.c
@@ -0,0 +1,61 @@
+
+#include <stdio.h>
+#include <string.h>
+
+//int main()
+//{
+//	int i = 0;
+//	printf("%d\n", !i);//
+//	i = 1;
+//	printf("%d\n", !i);
+//	return 0;
+//}
+
+//int main()
+//{
+//	int a = 3;
+//	int b = 2;
+//	printf("%f\n", a * 1.0 / b);//打印1.500000
+//	return 0;
+//}
+
+//int main()
+//{
+//	int a = 5;
+//	int b = 3;
+//	printf("%d\n", a / b);//打印1--商
+//	printf("%d\n", a % b);//打印2--余
+//}
+
+//int a = 20;
+//int main()
+//{
+//	int a = 10;
+//	printf("%d", a);//打印10
+//	return 0;
+//}
+
+//int b = 20;//在代码块{}外的变量是全局变量，变量名可以在文件中任何地方被使用
+//int main()
+//{
+//	int a = 10;//代码块内是局部变量，只能在代码块内使用，变量a在执行过程中，进入代码块创建，出代码块销毁
+//	printf("%d\n", a);//打印a的值后换行
+//	printf("%d\n", b);//打印b的值后换行 
+//	return 0;
+//}
+
+//int main()
+//{
+//	int a = 0;//在创建(定义)变量的时候赋值，是叫初始化，初始化的值叫初始值
+//	int b;//创建变量的时候，并没有赋值
+//	b = 0;//后面用赋值操作符叫赋值
+//	return 0;
+//}
+
+//int main()
+//{
+//	char arr[] = "hello, world";
+//	memset(arr, '#', 5);
+//	printf("%s\n", arr);
+//	return 0;
+//}
